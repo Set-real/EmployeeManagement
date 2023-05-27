9191e11a1ca3afc16356965a9cb4539508266c2c
Name <email@example.ru>
2023-05-27 15:44:13 +0300

    Добавьте файлы проекта.

---
+++ ViewModels/EmployeesViewModel.cs
@@ -0,0 +1,95 @@
+﻿using EmployeeManagement.Models;
+using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace EmployeeManagement.ViewModels
+{
+    public class EmployeesViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public EmployeeRepository _employeeRepository { get; }
+
+        public EmployeesViewModel()
+        {
+            _employeeRepository = new EmployeeRepository();
+            FillListView();
+            OnPropertyChanged();
+
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public ObservableCollection<Employee> _employees;
+        public ObservableCollection<Employee> Employees 
+        { get 
+            {
+                return _employees;
+            } 
+            set 
+            { 
+                _employees = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _filter;
+        public string Filter 
+        { get
+            {
+                return _filter;
+            }
+        set {
+                _filter = value; 
+                FillListView();
+            }
+        }
+
+        private string _filterMessage;
+        public string FilterMessage
+        {
+            get
+            {
+                return _filterMessage;
+            }
+            set
+            {
+                _filterMessage = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public void FillListView()
+        {
+            if(!string.IsNullOrEmpty(_filter))
+            {
+                _employees = new ObservableCollection<Employee>(
+                    _employeeRepository.GetAll().Where(x => x.FirstName.Contains(_filter)));
+            }
+            else
+            {
+                _employees = new ObservableCollection<Employee>(
+                    _employeeRepository.GetAll());
+            }
+        }
+
+        private void FillFilterMessage()
+        {
+            if (!String.IsNullOrEmpty(_filter))
+            {
+                FilterMessage = "По вашему запросу найдено: " + Employees.Count().ToString();
+            }
+            else
+            {
+                FilterMessage = "Введите данные для поиска";
+            }
+        }
+    }
+}
---
+++ Views/EmployessView.xaml.cs
@@ -0,0 +1,39 @@
+﻿using EmployeeManagement.Models;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace EmployeeManagement.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для EmployessView.xaml
+    /// </summary>
+    public partial class EmployessView : Window
+    {
+        public EmployessView()
+        {
+            InitializeComponent();
+        }
+
+        private void GridViewColumnHeader_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ListView_PreviewMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var item = (sender as ListView).SelectedItem;
+
+            if (item is null )
+            {
+                return;
+            }
+
+            var employee = item as Employee;
+
+            MessageBox.Show($"{employee.FirstName} {employee.LastName} Возраст: {employee.Age} Компания: {employee.CompanyName} " +
+                $"Должность: {employee.Position} Город: {employee.CityName}");
+            
+        }
+    }
+}
